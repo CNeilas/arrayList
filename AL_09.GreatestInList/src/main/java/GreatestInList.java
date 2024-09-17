ac8712bfe5864216fd5b93e588ed539b3d6f13ca
nsunklodas <farmeris108@gmail.com>
2024-09-17 13:46:02 +0300

    diddededededed

---
+++ AL_09.GreatestInList/src/main/java/GreatestInList.java
@@ -0,0 +1,28 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GreatestInList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+
+            list.add(input);
+        }
+
+        int maximum = list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+            if (maximum < list.get(i))
+                maximum = list.get(i);
+        }
+        System.out.println("Maximum Element in ArrayList = "
+                + maximum);
+    }
+}
